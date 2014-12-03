5c9fd92e1e2a346bf7ac59ed2ae9bec125afe16d
kjschabra <kjschabra@gmail.com>
2014-12-03 01:16:29 -0500

    Updated Splash Page
    Links working. Added the pre-order link

--- main.js
+++ main.js
@@ -11,34 +11,48 @@ $("document").ready(function(e){
 			$("div#color-images > div."+divclass+"").slideDown('slow');
 			clicked.addClass("active");
 
+
 		}
 	});
 
 	$("ul.navbar-nav > li").click(function(e){
 		$("ul.navbar-nav > li").removeClass('active');
 		$(this).addClass('active');
+		$("div#title").fadeTo('fast', 1.0);
+		$("div#ourstory").fadeTo('fast', 1.0);
+		$("div#customer").fadeTo('fast', 1.0);
+	});
+	$('a.navbar-brand').click(function(e){
+		$("ul.navbar-nav > li").removeClass('active');
+		$("ul.navbar-nav > li").eq(0).addClass('active');
 	});
 
 	$('html').on("mousewheel", function() {
         var val = $(document).scrollTop();
-        if (val < $("div.home").position().top - 150){
+        var x = 200;
+        if (val < $("div.home").position().top - x){
         	$("ul.navbar-nav > li").removeClass("active");
         	$("ul.navbar-nav > li").eq(0).addClass("active");
         	
         }
-        else if (val >= $("div.home").position().top - 150 && val < $("div#ourstory").position().top-150){
+        else if (val >= $("div.home").position().top - x && val < $("div#buy").position().top - x){
         	$("ul.navbar-nav > li").removeClass("active");
         	$("ul.navbar-nav > li").eq(1).addClass("active");
         	$("div#title").fadeTo('fast', 1.0);
         }
-        else if(val >= $("div#ourstory").position().top-150 && val < $("div#customer").position().top-150) {
+        else if (val >= $("div#buy").position().top - x && val < $("div#ourstory").position().top - x){
         	$("ul.navbar-nav > li").removeClass("active");
         	$("ul.navbar-nav > li").eq(2).addClass("active");
-        	$("div#ourstory").fadeTo('fast', 1.0);
+        	$("div#title").fadeTo('fast', 1.0);
         }
-        else if(val >= $("div#customer").position().top-150) {
+        else if(val >= $("div#ourstory").position().top - x && val < $("div#customer").position().top - x) {
         	$("ul.navbar-nav > li").removeClass("active");
         	$("ul.navbar-nav > li").eq(3).addClass("active");
+        	$("div#ourstory").fadeTo('fast', 1.0);
+        }
+        else if(val >= $("div#customer").position().top - x) {
+        	$("ul.navbar-nav > li").removeClass("active");
+        	$("ul.navbar-nav > li").eq(4).addClass("active");
         	$("div#customer").fadeTo('fast', 1.0);
         }
     });
